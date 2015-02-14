e7110a957a7f7a0da264bdd035fb5365b7b0b894
Magnus Feuer <mfeuer@jaguarlandrover.com>
2015-02-13 16:52:34 -0800

    Now does multiple taxi files

--- ats_taxi_simulator.py
+++ ats_taxi_simulator.py
@@ -21,6 +21,8 @@ import time
 from datetime import tzinfo, timedelta, datetime
 from signal import *
 from rvilib import RVI
+import Queue
+import threading
 MY_NAME = "Big Data Demo"
     
  
@@ -35,7 +37,7 @@ class UTC(tzinfo):
     def dst(self, dt):
         return timedelta(0)
 
-class DataSender():
+class TaxiFileReader():
     """
     Sends data from the database to RVI
     """
@@ -49,20 +51,29 @@ class DataSender():
     # loop_data - If set, start over with data_file once all elements have been sent
     # adjust_data - If set, use current time as time stamp offset by TS deltas in data file.
     # 
-    def __init__(self, destination, vin, data_file, rvi, speedup, loop_data, adjust_ts):
-        self.destination = destination
-        self.vin = vin
-        self.data_file = data_file
-        self.rvi = rvi
+    def __init__(self, file_name, data_sender, speedup, loop_data, adjust_ts):
+        self.data_sender = data_sender
         self.speedup = speedup
         self.loop_data = loop_data
         self.adjust_ts = adjust_ts
-        self.transaction_id = 1
         self.utc = UTC()
 
-    def shutdown(self):
-        self._Thread__stop()
+        # File name expected to be: 
+        # directory/.../new_[ID].txt'
+        # [ID] will be extracted and be used as VIN
+
+        # Remove any directory components, leaving just the filename.
+        short_name = os.path.basename(file_name)
         
+        # Extract VIN bit of file name
+        self.vin = short_name[4:len(short_name)-4]
+
+        self.data_file = open(file_name, "r")
+
+    def start(self):
+        self.serve_thread = threading.Thread(target=self.run)
+        self.serve_thread.start()
+
     #
     # Line format:
     #   LAT        LON     O  TS
@@ -74,14 +85,14 @@ class DataSender():
     # TS = Unix timestamp
     #
     def parse_next_line(self):
-        self.data_file.ext.split(" ")
-
+        return self.data_file.next().split(" ")
 
     def run(self):
-        
+        print "Will send for {}".format(self.vin)
         # Parse first line to get things going
         try: 
-            lat, lon, occupancy, timestamp =  self.data_file.next().split()
+            lat, lon, occupancy, timestamp =  self.parse_next_line()
+
         except StopIteration:
             return True
 
@@ -91,7 +102,7 @@ class DataSender():
         # time, we need to store that time.
         next_send_ts = 0
 
-        if adjust_ts == True:
+        if self.adjust_ts == True:
             ts_offset = int(time.time()) - timestamp
         else:
             ts_offset = 0
@@ -104,27 +115,14 @@ class DataSender():
             # Sleep until it is time to send the given data
             sleep_interval = (float(next_send_ts) - time.time()) / self.speedup
             if sleep_interval > 0.0:
-                print "Will sleep for {} seconds.".format(sleep_interval)
+                print "Will sleep {} for {} seconds.".format(self.vin, sleep_interval)
                 time.sleep(sleep_interval)
 
 
             # Transmit data
             utc_ts = datetime.fromtimestamp(timestamp + ts_offset, self.utc).isoformat()
-            param = [{
-                u'vin': self.vin,
-                u'timestamp': utc_ts,
-                u'data': [ { u'channel': 'location',
-                             u'value': { u'lat': lat, 
-                                         u'lon': lon,
-                                         u'alt': '0.0'
-                                     } } ]
-            }]
+            self.data_sender.queue_datapoint(self.vin, utc_ts, lat, lon, '0.0', '0')
 
-            print "Sending: {}".format(param)
-
-            rvi.message(self.destination + "/logging/report", param)
-
-            self.transaction_id += 1
             try:
                 lat, lon, occupancy, new_timestamp = self.data_file.next().split()
             except StopIteration:
@@ -149,17 +147,64 @@ class DataSender():
             new_timestamp = int(new_timestamp)
             next_send_ts = int(time.time()) + new_timestamp - timestamp
             timestamp = new_timestamp
-            
+    
+class DataSender():
+    """
+    Sends data from the database to RVI
+    """
+    
+    # destination - RVI service URL to send data to
+    # data_file - File object with lat lon occupancy and unix timestamps ts to transmit
+    # rvi_server - The rvi node object
+    # speedup - The speedup ratio to send data with. 1.0 = send at interval specced by
+    #           timestamps.
+    #
+    # loop_data - If set, start over with data_file once all elements have been sent
+    # adjust_data - If set, use current time as time stamp offset by TS deltas in data file.
+    # 
+    def __init__(self, destination, rvi):
+        self.destination = destination
+        self.rvi = rvi
+        self.transaction_id = 1
+        self.utc = UTC()
+        self.queue = Queue.Queue()
 
+
+    def queue_datapoint(self, vin, utc_ts, lat, lon, alt, occupancy):
+        self.queue.put((vin, utc_ts, lat, lon, alt, occupancy))
+
+    def run(self):
+        
+        while True:
+            (vin, utc_ts, lat, lon, alt, occupancy) = self.queue.get()
+
+            # occupancy not used - yet.
+            param = [{
+                u'vin': vin,
+                u'timestamp': utc_ts,
+                u'data': [ { u'channel': 'location',
+                             u'value': { u'lat': lat, 
+                                         u'lon': lon,
+                                         u'alt': alt
+                                     } } ]
+            }]
+
+            print "Sending to {}: {}".format(self.destination, param)
+
+            rvi.message(self.destination, param)
 
 def cleanup(*args):
     print "Caught signal:", args[0], "Shutting down..."
     # Not much to do here.
     sys.exit(0)
 
-
 def usage():
-    print "Usage: %s  [-l] [-s speedup] [-a] cab-data-file RVI-URL" % sys.argv[0]
+    print "Usage: %s [-l] [-s speedup] [-a] RVI-URL file ..." % sys.argv[0]
+    print "  RVI-URL      URL of RVI node to use."
+    print
+    print "  file ...     One or more cab spotting data files to send."
+    print "               Filename will be used as vin."
+    print 
     print "  -l           Loop data set once exhausted."
     print "               Default is to exit once complete."
     print 
@@ -182,12 +227,16 @@ if __name__ == "__main__":
     adjust_ts = False
     speedup = 1.0
     try:
-        optlist, [data_path, rvi_url] = getopt.getopt(sys.argv[1:], 'ls:a')
+        optlist, remain = getopt.getopt(sys.argv[1:], 'f:ls:a')
     except:
         print "Failed to parse arguments"
         usage()
 
+    if len(remain) < 2:
+        usage()
 
+    rvi_url = remain[0]
+    file_names = remain[1:]
 
     for o, a in optlist:
         if o == '-l':
@@ -202,23 +251,10 @@ if __name__ == "__main__":
         else:
             usage()
             
-
-
-    # File name expected to be: 
-    # directory/.../new_[ID].txt'
-    # [ID] will be extracted and be used as VIN
-
-    # Remove any directory components, leaving just the filename.
-    fname = os.path.basename(data_path)
-    
-    # Extract VIN bit of 
-    vin = fname[4:len(fname)-4]
-
     # Welcome message
     print "RVI Big Data Device Simulator"
     print "RVI Node:             ", rvi_url
-    print "Data File:            ", data_path
-    print "VIN:                  ", vin
+    print "File(s):              ", file_names
     print "Loop data:            ", loop_data
     print "Speedup:              ", speedup
     print "Adjust time stamp:    ", adjust_ts
@@ -227,19 +263,21 @@ if __name__ == "__main__":
     # Setup outbound JSON-RPC connection to the RVI Service Edge
     rvi = RVI(rvi_url)
 
-
-    f = open(data_path, "r")
-
-
-    
     # Setup data sender
-    data_sender = DataSender("jlr.com/backend", 
-                             vin, 
-                             f, 
-                             rvi, 
-                             speedup,
-                             loop_data,
-                             adjust_ts)
+    data_sender = DataSender("jlr.com/backend/logging/report", rvi)
+    
+
+    for file_name in file_names:
+        try:
+            file_reader = TaxiFileReader(file_name, 
+                                         data_sender, 
+                                         speedup, 
+                                         loop_data, 
+                                         adjust_ts)
+        except IOError:
+            print "Failed to open file {}. Skipped.".format(file_name)
+
+        file_reader.start()
 
 
     # catch signals for proper shutdown
